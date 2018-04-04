df3691e535bbd2b61c4231953ac0cc9bd9855596
nevenacolic <mrsnevenac@gmail.com>
2018-04-04 22:36:50 +0200

    json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
+import * as Rxjs from 'rxjs';
+import { interval } from 'rxjs/Observable/interval';
+
 const main = document.getElementById("main");
 
 const p = document.createElement('p');
 p.innerHTML = "test test test";
 
 main.appendChild(p);
-
-const p1 = document.createElement('p');
-p1.innerHTML = "promena";
-main.appendChild(p1);
